d9e40592659c63c1836c87258951f82052e09a8a
dharmendrasha <dharmendra.soni@scaletech.xyz>
2024-07-17 12:31:07 +0530

    fix: google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,25 @@
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
+import {
+	defineConfig
+} from "astro/config";
 
+import partytown from '@astrojs/partytown';
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  site: import.meta.env.DEV
-    ? "http://localhost:4321"
-    : "https://luna-landing-rust.vercel.app/",
-  integrations: [tailwind(), sitemap(), robotsTxt()],
+	site: import.meta.env.DEV ?
+		"http://localhost:4321" :
+		"https://dataminer.site/",
+	integrations: [
+		tailwind(),
+		sitemap(),
+		robotsTxt(),
+		partytown({
+			config: {
+				forward: ["dataLayer.push"],
+			},
+		})
+	],
 });
