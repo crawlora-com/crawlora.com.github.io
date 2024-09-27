8dea1c126ed4f2129cfba35f2362a0e06cc171ee
dharmendrasha <dharmendra.soni@scaletech.xyz>
2024-09-27 12:58:11 +0530

    fix: intigrated with post and the data

--- src/api/wp/sdk.ts
+++ src/api/wp/sdk.ts
@@ -1,3 +1,3 @@
 import WpApi from 'wpapi'
 
-export const wpSDK = new WpApi({endpoint: "http://88.222.244.129:8082/wp-json"})
+export const wpSDK = new WpApi({endpoint: "https://cms.crawlora.com//wp-json"})
